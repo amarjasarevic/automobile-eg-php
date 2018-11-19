695c17ed33d1cd993119fbe56fcae61339d23497
AmarJ-QA <amarjasha@gmail.com>
2018-11-19 20:43:40 +0100

    Fix email subject

--- web/send_email.php
+++ web/send_email.php
@@ -1,14 +1,14 @@
 <?php
 $to = "amarjasarevic@hotmail.com";
 
-$subject = "New car request";
+$subject = "Neuwagenanfrage";
 
 $_POST = json_decode(file_get_contents('php://input'), true);
 
 $headers = "From: Automobile AG\r\n";
 $headers .= "Reply-To: ". strip_tags($_POST['email']) . "\r\n";
 $headers .= "MIME-Version: 1.0\r\n";
-$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+$headers .= "Content-Type: text/html; charset=UTF-8\r\n";
 
 $message = "<html><body>";
 $message .= "<h1>Automobile EG</h1>";
